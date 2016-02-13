115da274879fb69e8c947ae96dcd0476d3523d2c
rohityadav876 <rhtdv04@gmail.com>
2016-02-13 19:33:55 +0530

    used to show user iff he is not authenticated.

---
+++ access-denied.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Student DashBoard:Access Denied</title>
+<link href="style/umain.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="page">
+  <div id="menu">
+  <ul>
+  <li><a href="http://www.gatevision.org"> Home</a></li>
+  <li><a href="http://www.gatevisiov.org/contactus.html">Contact Us</a></li>
+  </ul>
+  </div>
+<div id="header">
+<!-- Here We will give some images-->
+</div>
+<div id="center">
+  <h1>Access Denied</h1>
+  <div style="border:#bd6f2f solid 1px;padding:4px 6px 2px 6px">
+<div class="error"><img src="images/access-denied.png" width="300" height="300" alt=""/> </div>
+  <p>You don't have access to this page. <a href="index.php" style="font-size:18px;">Click Here</a> to login first or register for free. The registration won't take long:-)</p>
+  </div>
+</div>
+<div id="footer">
+    <div class="bottom_menu"><a href="http://www.gatevision.org/">Home Page</a>  |  <a href="http://www.gatevision.org/aboutus.html">About Us</a>
+	</div>
+  
+  <div class="bottom_addr">&copy; 2014-15. All Rights Reserved</div>
+</div>
+</div>
+</body>
+</html>
