62bf720daaf975d0514715c6a388935092a36a05
rohityadav876 <rhtdv04@gmail.com>
2016-02-13 19:35:01 +0530

    Access Denied page

---
+++ admin/access-denied.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Access Denied</title>
+<link href="main.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="page">
+<div class="header">
+<h1>Access Denied </h1>
+<p align="center">&nbsp;</p>
+</div><br>
+<h4 align="center" class="err"><img src="../images/access-denied.png" width="300" height="300" alt=""/><br />
+  You do not have access to this resource.</h4> <br>
+  <p align="center"><a href="login.php">Click Here</a> to login first.</p><br>
+  <?php require_once("ABottom.php");?>
+</div>
+</body>
+</html>
